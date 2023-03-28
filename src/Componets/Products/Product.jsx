9d67c64ac062302d7245a3a1e4187660eb18a44c
najmul905 <nh4808563@gmail.com>
2023-03-28 19:18:42 +0600

    add button

--- src/Componets/Products/Product.jsx
+++ src/Componets/Products/Product.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './Product.css'
 const Product = (props) => {
    
-    const {img,name,seller,price,quantity
+    const {img,name,seller,price,quantity,ratings
     }=props.product
     return (
         <div className='product'>
            <img src={img} alt="" />
+       <div className='product-info'>
+           <h4 className='name'>{name}</h4>
+           <p>Price:${price}</p>
+           <p>Manufacturer :{seller}</p>
+           <p>Ratting:{ratings}</p>
+       </div>
+       <button className='btn-cart'>Add to cart</button>
         </div>
     );
 };
